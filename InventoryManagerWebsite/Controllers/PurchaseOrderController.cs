13333c4047d2c520faab027a5d5b80dae9a4d42f
Rudy Wilkinson Jr <rudywilkjr@gmail.com>
2018-08-08 22:21:58 -0400

    New Purchase Orders view.  Featuring expand/collapse grid of Invoice Products.

---
+++ InventoryManagerWebsite/Controllers/PurchaseOrderController.cs
@@ -0,0 +1,27 @@
+﻿using InventoryManagerService.Invoice;
+using InventoryManagerWebsite.Models.Invoice;
+using InventoryManagerWebsite.Models.PurchaseOrder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoryManagerWebsite.Controllers
+{
+    public class PurchaseOrderController : Controller
+    {
+        private readonly InvoiceService _invoiceService = new InvoiceService();
+
+        // GET: PurchaseOrder
+        public ActionResult Index()
+        {
+            var purchaseOrderViewModel = new PurchaseOrderViewModel
+            {
+                PurchaseOrders = AutoMapper.Mapper.Map<ICollection<InvoiceModel>>(_invoiceService.GetInvoices(string.Empty))
+            };
+
+            return View("View", purchaseOrderViewModel );
+        }
+    }
+}
---
+++ InventoryManagerWebsite/Models/PurchaseOrder/PurchaseOrderViewModel.cs
@@ -0,0 +1,14 @@
+﻿using InventoryManagerWebsite.Models.Invoice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryManagerWebsite.Models.PurchaseOrder
+{
+    public class PurchaseOrderViewModel
+    {
+        public ICollection<InvoiceModel> PurchaseOrders { get; set; }
+
+    }
+}
